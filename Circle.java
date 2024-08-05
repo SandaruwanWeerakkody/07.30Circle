94a7db058d490a148af4c4df9e7a3412a7197036
Sandaruwan Weerakkody <154538944+SandaruwanWeerakkody@users.noreply.github.com>
2024-08-05 20:14:56 +0530

    Create Circle.java

---
+++ Circle.java
@@ -0,0 +1,27 @@
+public class Circle {
+
+    private double radius;
+    private String colour;
+
+    public Circle(){
+        radius = 5.4;
+        colour = "Red";
+    }
+    public Circle(double r){
+        radius = r;
+        colour ="Red";
+    }
+    public Circle(double r,String c){
+        radius = r;
+        colour = c;
+    }
+    public double getRadius(){
+        return radius;
+    }
+    public String getColour(){
+        return colour;
+    }
+    public double getArea(){
+        return 3.14*radius*radius;
+    }
+}
